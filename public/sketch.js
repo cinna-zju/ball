3587a2a0f42c193f78f2a461765f2e5745d48694
Siba Siddique <siba@nyu.edu>
2019-01-11 16:58:38 +0100

    ADD map()

--- public/sketch.js
+++ public/sketch.js
@@ -1,33 +1,27 @@
 var socket; 
-var a1, b1, c1, a2, b2, c2;
+var a1, b1, c1, a2, b2, c2, angle, slope;
 var id1="", id2="";
 function setup() {
   createCanvas(720, 480);
   socket = io();
   socket.on('get', function (msg) {
-    if (id1 === "") {
-      id1 = msg.id;
-    } else {
-      if (id1 !== msg.id) {
-        id2 = msg.id;
-      }
-      if (id1 === msg.id) {
+
         a1 = msg.alpha;
         b1 = msg.beta;
         c1 = msg.gamma;
-      }
-    }
 
-    if (id2 === msg.id) {
-      a2 = msg.alpha;
-      b2 = msg.beta;
-      c2 = msg.gamma;     
-    }
+})
+}
 
 
-  })
 
+//define function to do mapping
+// from [x1,x2] to [y1,y2]
+function map(input, x1, x2, y1, y2){
+  slope = 1.0 * (y2 - y1) / (x2 - x1);
+  output = y1 + slope * (input - x1);
 
+  return angle;
 }
 
 function draw() {
@@ -37,8 +31,10 @@ function draw() {
   // console.log("id1" + id1);
   // console.log("id2" + id2);
   
-  ellipse(360 - b1, 240 - a1 / 2, 30, 30);
-  rect(360 - b2, 240 - a2/2 , 50, 50);    
+  out_angle = map(c1, -80, 80, 0, 720);
+  ellipse(out_angle, 240 , 30, 30);
+  //ellipse(360-c2, 240 , 30, 30);
+  // rect(360 - b2, 240, 50, 50);    
   
   
 }
