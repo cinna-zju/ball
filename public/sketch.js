5286d9df29588c91a7a742558e3a2d0dc8222a68
cinna-zju <569095288@qq.com>
2019-01-18 17:36:28 +0100

    ADD lock

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,7 @@
 
 var socket; 
-var a1, b1, c1, a2, b2, c2, angle, slope, color, isSave;
+var a1, b1, c1, a2, b2, c2, angle, slope, color, isSave, out_angle;
+let isLock;
 var id1 = "", id2 = "";
 
 var gif;
@@ -10,6 +11,7 @@ function setup() {
   myCanvas.parent('canvas');
   color = 'red';
   socket = io();
+  out_angle = 400;
   socket.on('get', function (msg) {
     //TODO: for multiuser
     // if (id1 === "") {
@@ -37,6 +39,7 @@ function setup() {
     accX = msg.accX;
     color = msg.color;
     isSave = msg.isSave;
+    isLock = msg.isLock;
 
   })
 }
@@ -53,8 +56,11 @@ function map(input, x1, x2, y1, y2){
 function draw() {
   clear();
   background(255);
-  out_angle = map(c, -80, 80, 0, 720);
-  ellipse(out_angle, 240, 30, 30);
+  if (isLock === false) {
+    out_angle = map(c, -80, 80, 0, 720);
+  }
+  ellipse(out_angle, 240, 30, 30);    
+
   if (accX < -8) {
     switch (color) {
       case 'red':
@@ -62,11 +68,11 @@ function draw() {
       case 'yellow':
         gif = createImg('./Y1.gif'); break;
     }
-    gif.position(out_angle - 400, 140);
+    gif.position(out_angle - 180, 200);
   }
 
   if (isSave === true) {
-    //saveCanvas('myCanva', 'jpg');
+    saveCanvas('myCanva', 'jpg');
     isSave = false;
   }
 
