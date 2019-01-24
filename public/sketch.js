7da0ed5a4b8d9288d5d9ec842ff05fcfc82b7d92
cinna-zju <569095288@qq.com>
2019-01-24 20:42:11 +0100

    ADD multiplayer

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,9 @@
-
 var socket; 
-var a1, b1, c1, a2, b2, c2, angle, slope, color, isSave, out_angleX1, out_angleY, accZ;
-let isLock;
-var id1 = "", id2 = "";
+var a1, b1, c1, color1, out_angleX1, out_angleY1, accX1, accZ1;
+var a2, b2, c2, color2, out_angleX2, out_angleY2, accX2, accZ2; 
+let isLock1, isLock2, isSave;
 
-var gif;
+var gif1, gif2;
 
 function setup() {
   var myCanvas = createCanvas(800, 500);
@@ -13,43 +12,33 @@ function setup() {
   socket = io();
   out_angleX1 = 400;
   out_angleY = 250;
-  socket.on('get', function (msg) {
-    //TODO: for multiuser
-    // if (id1 === "") {
-    //   id1 = msg.id;
-    // } else {
-    //   if (id1 !== msg.id) {
-    //     id2 = msg.id;
-    //   }
-    //   if (id1 === msg.id) {
-    //     a1 = msg.alpha;
-    //     b1 = msg.beta;
-    //     c1 = msg.gamma;
-    //   }
-    // }
+  socket.on('user1', function (msg) {
+    a1 = msg.alpha;
+    b1 = msg.beta;
+    c1 = msg.gamma;
+    accX1 = msg.accX;
+    accZ1 = msg.accZ;
+    color1 = msg.color;
+    // isSave = msg.isSave;
+    isLock1 = msg.isLock;
+  })
 
-    // if (id2 === msg.id) {
-    //   a2 = msg.alpha;
-    //   b2 = msg.beta;
-    //   c2 = msg.gamma;     
-    // }
-
-    a = msg.alpha;
-    b = msg.beta;
-    c = msg.gamma;
-    accX = msg.accX;
-    accZ = msg.accZ;
-    color = msg.color;
-    isSave = msg.isSave;
-    isLock = msg.isLock;
-
+  socket.on('user2', function (msg) {
+    a2 = msg.alpha;
+    b2 = msg.beta;
+    c2 = msg.gamma;
+    accX2 = msg.accX;
+    accZ2 = msg.accZ;
+    color2 = msg.color;
+    // isSave = msg.isSave;
+    isLock2 = msg.isLock;
   })
 }
 
 //define function to do mapping
 // from [x1,x2] to [y1,y2]
 function map(input, x1, x2, y1, y2){
-  slope = 1.0 * (y2 - y1) / (x2 - x1);
+  var slope = 1.0 * (y2 - y1) / (x2 - x1);
   output = y1 + slope * (input - x1);
 
   return angle;
@@ -58,16 +47,26 @@ function map(input, x1, x2, y1, y2){
 function draw() {
   clear();
   background(255);
-  if (isLock == false) {
-    if(a >=0 && a<=90){
-      out_angleX1 = map(a, 90, 0, 0, 400); //initial range across alpha (0, 400)
+  if (isLock1 === false) {
+    if(a1 >=0 && a1<=90){
+      out_angleX1 = map(a1, 90, 0, 0, 400); //initial range across alpha (0, 400)
     }
-    if(a <= 360 && a >= 270){
-    out_angleX1 = map(a, 360, 270, 400, 800);
+    if(a1 <= 360 && a1 >= 270){
+      out_angleX1 = map(a1, 360, 270, 400, 800);
 
     }
-    out_angleY = map(b, 80, -80, 0, 500);
-   
+    out_angleY1 = map(b1, 80, -80, 0, 500);
+  }
+
+  if (isLock2 === false) {
+    if(a2 >=0 && a2<=90){
+      out_angleX2 = map(a2, 90, 0, 0, 400); //initial range across alpha (0, 400)
+    }
+    if(a2 <= 360 && a2 >= 270){
+    out_angleX2 = map(a2, 360, 270, 400, 800);
+
+    }
+    out_angleY2 = map(b2, 80, -80, 0, 500);
   }
 
  /* added if condition for x-axis range
@@ -77,17 +76,22 @@ function draw() {
     ellipse(out_angleX2, 240, 30, 30);  
  ellipse(400, out_angleY, 30, 30);    // test y-axis range */
 
-  ellipse(out_angleX1, out_angleY, 30, 30);
+  ellipse(out_angleX1, out_angleY1, 30, 30);
+  rect(out_angleX2, out_angleY2, 30, 30);
   // ellipse(0,0,30,30);
 
 
-  if (accX < -5) { //reduce accel threshold!
-    switch (color) {
+  if (accX1 < -5) { //reduce accel threshold!
+    switch (color1) {
       case 'red':
-        
-        gif = createImg('./R1.gif'); break;
+        if (accZ1 > 3) {
+          gif1 = createImg('.GIF/R right.gif');
+        } else {
+          gif1 = createImg('./R1.gif');
+        }
+        break;
       case 'yellow':
-        gif = createImg('./Y1.gif'); break;
+        gif1 = createImg('./Y1.gif'); break;
 
         // add case for blue
         /*
@@ -95,7 +99,28 @@ function draw() {
         gif = createImg('./B1.gif'); break;
         */
     }
-    gif.position(out_angleX1 - 180, out_angleY);
+    gif1.position(out_angleX1 - 180, out_angleY1);
+  }
+
+  if (accX2 < -5) { //reduce accel threshold!
+    switch (color2) {
+      case 'red':
+        if (accZ2 > 3) {
+          gif2 = createImg('.GIF/R right.gif');
+        } else {
+          gif2 = createImg('./R1.gif');
+        }
+        break;
+      case 'yellow':
+        gif2 = createImg('./Y1.gif'); break;
+
+        // add case for blue
+        /*
+      case 'blue':
+        gif = createImg('./B1.gif'); break;
+        */
+    }
+    gif2.position(out_angleX2 - 180, out_angleY2);
   }
 
   if (isSave === true) {
